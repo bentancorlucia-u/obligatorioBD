96aae10c1b71382d987a2e854f6f6706d6e1a3e0
lucia <bentancorlucia.u@gmail.com>
2025-11-23 22:26:09 -0300

    users_template

---
+++ sql/users_template.sql
@@ -0,0 +1,5 @@
+-- creacion de usuarios
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY '*******';
+CREATE USER IF NOT EXISTS 'administrativo'@'%' IDENTIFIED BY '*******';
+CREATE USER IF NOT EXISTS 'login'@'%' IDENTIFIED BY '*******';
+CREATE USER IF NOT EXISTS 'reportes'@'%' IDENTIFIED BY '*******';
